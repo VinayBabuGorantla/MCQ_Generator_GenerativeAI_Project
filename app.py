0a2c75960b555e518390b0f664de5101cf4fc8de
vinay <vinayc.gorantla@gmail.com>
2023-12-25 23:42:58 +0000

    Interim Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+import json
+import pandas as pd
+import traceback
+from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file,get_table_data
+from src.mcqgenerator.logger import logging
+import streamlit as st
+
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+from langchain.callbacks import get_openai_callback
+import PyPDF2
+
