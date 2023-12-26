da8ca3919d0517cc22ab44858b4a0d6e0d20eaee
vinay <vinayc.gorantla@gmail.com>
2023-12-26 00:15:58 +0000

    Interim Commit

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import os
 import json
 import traceback
 import pandas as pd
-from dotenv import load_dotenv
-from src.mcqGenerator.utils import read_file,get_table_data
+#from dotenv import load_dotenv
+from src.mcqgenerator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from src.mcqgenerator.mcqenerator import generate_evaluate_chain
-from src.mcqGenerator.logger import logging
+from src.mcqgenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
 
 #loading json file
 
